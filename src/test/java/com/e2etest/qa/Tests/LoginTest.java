a5203ce43d2bf95137fa90c18497461f09db955b
pselva862 <pselva862@APAC-4GQ10J3>
2024-06-17 17:53:02 +0530

    Created Hybrid TestNG framework which includes POM and Page Factory pattern.

---
+++ src/test/java/com/e2etest/qa/Tests/LoginTest.java
@@ -0,0 +1,25 @@
+package com.e2etest.qa.Tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	public static WebDriver driver;
+
+	@Test
+	public static void initializeBrowser() {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("http://tutorialsninja.com/demo/");
+	}
+
+}
